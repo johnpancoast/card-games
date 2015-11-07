c55e9b118c3b5d1dd6e332487a8288c4b479a210
John Pancoast <shideon@gmail.com>
2015-11-07 15:01:45 -0800

    Added exception for game logic issues

---
+++ src/Exception/GameLogicException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package johnpancoast/card-games
+ * @copyright (c) 2015 John Pancoast
+ * @author John Pancoast <johnpancoaster@gmail.com>
+ * @license MIT
+ */
+
+namespace Pancoast\CardGames\Exception;
+
+/**
+ * An exception in game logic
+ *
+ * @author John Pancoast <johnpancoaster@gmail.com>
+ */
+class GameLogicException extends \Exception
+{
+}
