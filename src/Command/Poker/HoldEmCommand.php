b48e2699de0d01805515a30849239e3927585660
John Pancoast <shideon@gmail.com>
2015-11-07 10:57:38 -0800

    Added command hold em

---
+++ src/Command/Poker/HoldEmCommand.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package johnpancoast/card-games
+ * @copyright (c) 2015 John Pancoast
+ * @author John Pancoast <johnpancoaster@gmail.com>
+ * @license ${LICENSE}
+ */
+
+namespace Pancoast\CardGames\Command\Poker;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @todo Add description
+ *
+ * @author John Pancoast <johnpancoaster@gmail.com>
+ */
+class HoldEmCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('demo:greet')
+            ->setDescription('Greet someone')
+            ->addArgument(
+                'name',
+                InputArgument::OPTIONAL,
+                'Who do you want to greet?'
+            )
+            ->addOption(
+                'yell',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, the task will yell in uppercase letters'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        if ($name) {
+            $text = 'Hello '.$name;
+        } else {
+            $text = 'Hello';
+        }
+
+        if ($input->getOption('yell')) {
+            $text = strtoupper($text);
+        }
+
+        $output->writeln($text);
+    }
+}
