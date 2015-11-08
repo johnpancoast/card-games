598838bc054a0cc589293d4dfe4d51f6497d9f05
John Pancoast <shideon@gmail.com>
2015-11-08 10:52:47 -0800

    Added core hold-em logic

--- src/Game/Poker/HoldEm.php
+++ src/Game/Poker/HoldEm.php
@@ -14,6 +14,7 @@ use Pancoast\CardGames\Card\CardCollection;
 use Pancoast\CardGames\Card\CardInterface;
 use Pancoast\CardGames\Card\CardNumber;
 use Pancoast\CardGames\Card\CardSuit;
+use Pancoast\CardGames\Card\PokerHandInterface;
 use Pancoast\CardGames\CardGameInterface;
 use Pancoast\CardGames\Exception\GameLogicException;
 use Pancoast\CardGames\Game\StepCollection;
@@ -21,7 +22,10 @@ use Pancoast\CardGames\People\PlayerInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * @todo Add description
+ * Texas Hold 'Em
+ *
+ * Well, actually it's just a short version of it but the structure is
+ * here to build the actual game if you wanted. =)
  *
  * @author John Pancoast <johnpancoaster@gmail.com>
  */
@@ -37,6 +41,11 @@ class HoldEm extends AbstractCardGame
      */
     private $deck;
 
+    /**
+     * Constructor
+     *
+     * @param OutputInterface $output
+     */
     public function __construct(OutputInterface $output)
     {
         parent::__construct($output);
@@ -51,10 +60,17 @@ class HoldEm extends AbstractCardGame
     public function getStepCollection()
     {
         $collection = new StepCollection();
-        $collection->add([$this, '_deal']);
+        $collection->add([$this, 'deal']);
+        // example of what we could've done...
+        // $collection->add([$this, 'dealFlop']);
+        // $collection->add([$this, 'handleBets']);
+        // $collection->add([$this, 'dealTurn']);
+        // $collection->add([$this, 'handleBets']);
+        // $collection->add([$this, 'dealRiver']);
+        // $collection->add([$this, 'handleBets']);
+        $collection->add([$this, 'outputWinner']);
+
         return $collection;
-        // @todo
-        //$collection->add(<foo>);
     }
 
     /**
@@ -112,7 +128,7 @@ class HoldEm extends AbstractCardGame
      * @param CardInterface $card
      * @throws \Exception
      */
-    private function dealToCommunity(CardInterface $card)
+    private function dealCardToCommunity(CardInterface $card)
     {
         // attempt to clean things as much as possible for now
         try {
@@ -123,6 +139,20 @@ class HoldEm extends AbstractCardGame
             $this->communityCards->removeCard($card);
 
             throw $e;
+        }
+    }
+
+    /**
+     * Assert that the deck has a certain amount of cards
+     *
+     * @param int $expectedAmount
+     * @throws GameLogicException If deck count is not the expected amount
+     * @todo Obviously this (along with many other tests) would live in src/Tests/ but I didn't want to spend my time testing a test project!
+     */
+    private function assertDeckCount($expectedAmount)
+    {
+        if ($this->deck->count() != $expectedAmount) {
+            throw new GameLogicException(sprintf("Should have %s cards in deck but have %s", $expectedAmount, $this->deck->count()));
         }
     }
 
@@ -140,58 +170,68 @@ class HoldEm extends AbstractCardGame
      * @throws GameLogicException
      * @throws \Exception
      */
-    protected function _deal(CardGameInterface $cardGame, OutputInterface $output)
+    protected function deal(CardGameInterface $cardGame, OutputInterface $output)
     {
         $this->buildAndShuffleDeck();
+        $deckCount = $this->deck->count();
 
-        // the practical never talked about order of deal (and i'm used to the normal hold 'em
-        // where you deal the flop first etc).
-        //
-        // here are its words:
-        // The program should deal 2 "hole" cards to each player and 5 "community" cards.
-        //
-        // so that's what we'll do!
+        // practical's words:
+        // "The program should deal 2 "hole" cards to each player and 5 "community" cards."
+        $playerCardCount = 2;
+        $communityCardCount = 5;
+
+        // deal to players
         foreach ($cardGame->getPlayers() as $player) {
-            for ($i = 1; $i <= 2; $i++) {
+            for ($i = 1; $i <= $playerCardCount; $i++) {
                 $card = $this->deck->current();
-
-                // attempt to clean things as much as possible for now
-                try {
-                    $this->deck->removeCard($card);
-                    $player->addToHand($card);
-                    $this->deck->next();
-                } catch (\Exception $e) {
-                    $this->deck->addCard($card);
-                    $player->removeFromHand($card);
-
-                    throw $e;
-                }
+                $this->dealCardToPlayer($player, $card);
             }
         }
 
-        if ($this->deck->count() != 48) {
-            throw new GameLogicException(sprintf("Should have 48 cards in deck but have %s", $this->deck->count()));
-        }
-
-        // deal community
-        for ($i = 1; $i <= 5; $i++) {
+        // deal community cards
+        $this->deck->first();
+        for ($i = 1; $i <= $communityCardCount; $i++) {
             $card = $this->deck->current();
-
-            // attempt to clean things as much as possible for now
-            try {
-                $this->deck->removeCard($card);
-                $this->communityCards->addCard($card);
-                $this->deck->next();
-            } catch (\Exception $e) {
-                $this->deck->addCard($card);
-                $this->communityCards->removeCard($card);
-
-                throw $e;
-            }
+            $this->dealCardToCommunity($card);
         }
 
-        if ($this->deck->count() != 43) {
-            throw new GameLogicException(sprintf("Should have 43 cards in deck but have %s", $this->deck->count()));
+        $this->assertDeckCount($deckCount - ($cardGame->getPlayers()->count() * $playerCardCount) - $communityCardCount);
+    }
+
+    /**
+     * Determine and output the winner
+     *
+     * @param CardGameInterface $cardGame
+     * @param OutputInterface $output
+     * @throws GameLogicException
+     */
+    protected function outputWinner(CardGameInterface $cardGame, OutputInterface $output)
+    {
+        $players = [];
+
+        // combine player hands with community cards to make actual hands
+        foreach ($cardGame->getPlayers() as $player) {
+            $hand = clone $player->getHand();
+
+            foreach ($this->communityCards as $card) {
+                $hand->addCard($card);
+            }
+
+            if ($hand->count() != 7) {
+                throw new GameLogicException(sprintf('Players hand should have 7 cards but it has %s', $hand->count()));
+            }
+
+            $players[$player->getId()] = $hand;
+        }
+
+        foreach ($players as $playerId => $hand) {
+            $pokerHand = $hand->getPokerHand();
+
+            if (!$pokerHand instanceof PokerHandInterface) {
+                throw new GameLogicException('Expecting a poker hand');
+            }
+
+            $output->writeln(sprintf("Player %s has a %s with cards [%s]", $playerId, $pokerHand->getType(), (string)$pokerHand));
         }
     }
 }
